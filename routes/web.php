c6bff076f49fd2d036cae5a64c1b04be1e9b4314
rafliaryansyah <raflyaryansha86@gmail.com>
2020-06-29 05:43:32 +0700

    admin stisla

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', 'DashboardController@index');
+Route::get('/welcome', 'WelcomeController@index');
+Route::get('/choose', 'ChooseController@index');
+Route::get('/history', 'HistoryController@index');
+Route::get('/news', 'NewsController@index');
