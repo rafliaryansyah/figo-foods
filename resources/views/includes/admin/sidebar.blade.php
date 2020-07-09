acc3449458120ca7fa526a05a1d127f872296478
rafliaryansyah <raflyaryansha86@gmail.com>
2020-07-09 16:32:18 +0700

    active state sidebat

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -8,23 +8,23 @@
       </div>
       <ul class="sidebar-menu">
         <li class="menu-header">Dashboard</li>
-        <li class="dropdown active">
-          <a href="{{ url('/dashboard') }}" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+        <li class="dropdown{{ request()->is('dashboard') ? ' active' : '' }}">
+          <a href="{{ url('/dashboard') }}" class="nav-link active"><i class="fas fa-fire"></i><span>Dashboard</span></a>
         </li>
         <li class="menu-header">Welcome</li>
-        <li class="dropdown">
+        <li class="dropdown{{ request()->is('welcome') ? ' active' : '' }}">
           <a href="{{ url('/welcome') }}" class="nav-link"><i class="fas fa-door-open"></i> <span>Welcome</span></a>
         </li>
         <li class="menu-header">Why Choose Us</li>
-        <li class="dropdown">
+        <li class="dropdown{{ request()->is('choose') ? ' active' : '' }}">
           <a href="{{ url('/choose') }}" class="nav-link"><i class="fas fa-thumbtack"></i> <span>Why Choose Us</span></a>
         </li>
         <li class="menu-header">History</li>
-        <li class="dropdown">
+        <li class="dropdown{{ request()->is('history') ? ' active' : '' }}">
           <a href="{{ url('/history') }}" class="nav-link"><i class="fas fa-history"></i> <span>History</span></a>
         </li>
         <li class="menu-header">Latest News</li>
-        <li class="dropdown">
+        <li class="dropdown{{ request()->is('news') ? ' active' : '' }}">
           <a href="{{ url('/news') }}" class="nav-link"><i class="fas fa-newspaper"></i> <span>Latest News</span></a>
         </li>
         <li class="menu-header">Footer</li>
